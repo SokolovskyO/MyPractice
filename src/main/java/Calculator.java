da5aad3e5c988e28f97c6a8e09d559b3d2bc8a39
ripperbnet <skatabardaok@gmail.com>
2023-08-24 21:30:58 +0300

    исправления четвертого задания

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,5 @@
 import org.junit.jupiter.api.Test;
-import java.util.Scanner;
-
 import static junit.framework.Assert.assertEquals;
-
 
 /**
  Created by Oleg Sokolovsky
@@ -10,121 +7,77 @@ import static junit.framework.Assert.assertEquals;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+    private String operation;
 
-        Scanner sc = new Scanner(System.in);
-
-        CalculatorNumbers calculatorNumbers = new CalculatorNumbers();
-
-        Calculator calc = new Calculator();
-
-        System.out.println("Введите операцию на выбор: + - * /");
-        String operation = sc.next();
-
-        try {
-            if (operation.equals("+")) {
-                System.out.println("Введите первое число:");
-                calculatorNumbers.setX(sc.nextLong());
-
-                System.out.println("Введите второе число:");
-                calculatorNumbers.setY(sc.nextLong());
-
-                System.out.println(calc.plus(calculatorNumbers.getX(), calculatorNumbers.getY()));
-            } else if (operation.equals("-")) {
-                System.out.println("Введите первое число:");
-                calculatorNumbers.setX(sc.nextLong());
-
-                System.out.println("Введите второе число:");
-                calculatorNumbers.setY(sc.nextLong());
-
-                System.out.println(calc.minus(calculatorNumbers.getX(), calculatorNumbers.getY()));
-            } else if (operation.equals("*")) {
-                System.out.println("Введите первое число:");
-                calculatorNumbers.setX(sc.nextLong());
-
-                System.out.println("Введите второе число:");
-                calculatorNumbers.setY(sc.nextLong());
-
-                System.out.println(calc.multiplication(calculatorNumbers.getX(), calculatorNumbers.getY()));
-            } else if (operation.equals("/")) {
-                System.out.println("Введите первое число:");
-                calculatorNumbers.setX(sc.nextLong());
-
-                System.out.println("Введите второе число:");
-                calculatorNumbers.setY(sc.nextLong());
-
-                System.out.println(calc.division(calculatorNumbers.getX(), calculatorNumbers.getY()));
-            } else {
-                System.out.println("Выбрана неверная операция");
-            }
-        } catch (ArithmeticException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-     long plus(long x, long y) {
-        return x + y;
-    }
-
-     long minus(long x, long y) {
-        return x - y;
-    }
-
-    long division(long x, long y) {
-        return x / y;
-    }
-
-    long multiplication(long x, long y) {
-        return x * y;
-    }
-
-} class CalculatorNumbers {
     private long x;
 
     private long y;
 
-    public long getX() {
-        return x;
-    }
+    private long result;
 
-    public long getY() {
-        return y;
-    }
-
-    public void setX(long x) {
+    public Calculator(String operation, long x, long y) {
+        this.operation = operation;
         this.x = x;
-    }
-
-    public void setY(long y) {
         this.y = y;
     }
 
+    public long getResult() {
+        return result;
+    }
+
+    public void setResult(long result) {
+        this.result = result;
+    }
+
+    public void calculateResult() {
+        try {
+            if (operation.equals("+")) {
+
+                setResult(x + y);
+            } else if (operation.equals("-")) {
+
+                setResult(x - y);
+            } else if (operation.equals("*")) {
+
+                setResult(x * y);
+            } else if (operation.equals("/")) {
+
+                setResult(x / y);
+            }
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 class  CalculatorTests {
 
     @Test
     public void calculatorMinusTest() {
-        Calculator calc = new Calculator();
-        assertEquals(0, calc.minus(100, 100));
+        Calculator calc = new Calculator("-", 100, 100);
+        calc.calculateResult();
+        assertEquals(0, calc.getResult());
     }
 
     @Test
     public void calculatorPlusTest() {
-        Calculator calc = new Calculator();
-        assertEquals(200, calc.plus(100, 100));
+        Calculator calc = new Calculator("+", 100, 100);
+        calc.calculateResult();
+        assertEquals(200, calc.getResult());
     }
 
     @Test
     public void calculatorDivisionTest() {
-        Calculator calc = new Calculator();
-        assertEquals(1, calc.division(100, 100));
+        Calculator calc = new Calculator("/", 100, 100);
+        calc.calculateResult();
+        assertEquals(1, calc.getResult());
     }
 
     @Test
     public void calculatorMultiplicationTest() {
-        Calculator calc = new Calculator();
-        assertEquals(10000, calc.multiplication(100, 100));
+        Calculator calc = new Calculator("*", 100, 100);
+        calc.calculateResult();
+        assertEquals(10000, calc.getResult());
     }
 
 }
