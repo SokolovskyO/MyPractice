bd3825e926e9ca759c6c7361204dfdce161115cd
ripperbnet <skatabardaok@gmail.com>
2023-08-25 22:04:10 +0300

    исправил калькулятор и тесты

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Test;
-import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  Created by Oleg Sokolovsky
@@ -9,28 +9,28 @@ public class Calculator {
 
     private String operation;
 
-    private long x;
+    private double x;
 
-    private long y;
+    private double y;
 
-    private long result;
+    private double result;
 
-    public Calculator(String operation, long x, long y) {
+    public Calculator(String operation, double x, double y) {
         this.operation = operation;
         this.x = x;
         this.y = y;
     }
 
-    public long getResult() {
+    public double getResult() {
         return result;
     }
 
-    public void setResult(long result) {
+    public void setResult(double result) {
         this.result = result;
     }
 
-    public void calculateResult() {
-        try {
+    public void calculateResult() throws Exception {
+
             if (operation.equals("+")) {
 
                 setResult(x + y);
@@ -43,41 +43,67 @@ public class Calculator {
             } else if (operation.equals("/")) {
 
                 setResult(x / y);
+
+                if (Double.isInfinite(getResult())) {
+                    throw new Exception("Ошибка: на ноль делить нельзя");
+                }
+            }  else if (!operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")) {
+                throw new Exception("Ошибка: выбрана неверная математическая операция");
             }
-        } catch (ArithmeticException e) {
-            e.printStackTrace();
-        }
-    }
+
+
 }
 
-class  CalculatorTests {
+static class CalculatorTests {
 
     @Test
-    public void calculatorMinusTest() {
+    public void exceptionDivisionOnZeroTest() throws Throwable {
+        try {
+            Calculator calc = new Calculator("/", 9.0, 0);
+        calc.getResult();
+        }
+        catch (Exception e) {
+            assertEquals(e.getMessage(), "Ошибка: на ноль делить нельзя");
+        }
+    }
+
+    @Test
+    public void exceptionWrongOperationTest() throws Throwable {
+        try {
+            Calculator calc = new Calculator("$", 10, 10);
+            calc.getResult();
+        }
+        catch (Exception e) {
+            assertEquals(e.getMessage(), "Ошибка: выбрана неверная математическая операция");
+        }
+    }
+
+    @Test
+    public void calculatorMinusTest() throws Throwable {
         Calculator calc = new Calculator("-", 100, 100);
         calc.calculateResult();
         assertEquals(0, calc.getResult());
     }
 
     @Test
-    public void calculatorPlusTest() {
+    public void calculatorPlusTest() throws Throwable {
         Calculator calc = new Calculator("+", 100, 100);
         calc.calculateResult();
         assertEquals(200, calc.getResult());
     }
 
     @Test
-    public void calculatorDivisionTest() {
+    public void calculatorDivisionTest() throws Throwable {
         Calculator calc = new Calculator("/", 100, 100);
         calc.calculateResult();
         assertEquals(1, calc.getResult());
     }
 
     @Test
-    public void calculatorMultiplicationTest() {
-        Calculator calc = new Calculator("*", 100, 100);
+    public void calculatorMultiplicationTest() throws Throwable {
+        Calculator calc = new Calculator("*", 0, 10);
         calc.calculateResult();
         assertEquals(10000, calc.getResult());
     }
-
+    }
 }
